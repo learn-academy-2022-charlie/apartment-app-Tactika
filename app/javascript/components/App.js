cca445eeb3b3d95f91dc564adacc299fc210b221
Damien Richcreek <damien@richcreek.dev>
2022-07-07 12:00:16 -0700

    Added homepage with tests and css

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -14,23 +14,39 @@ import {
 } from 'react-router-dom'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      apartments : []
+    }
+  }
+
+  readApartments = () => {
+    fetch("/apartments")
+    .then(response => response.json())
+    .then(payload => this.setState({apartments: payload}))
+    .catch(errors => console.log(errors))
+  }
+  componentDidMount(){
+    this.readApartments()
+  }
   render() {
+    console.log(this.state.apartments)
     return (
-      
+
         <Router>
-          <Header {...this.props} />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/apartmentindex" component={ApartmentIndex} />
-            <Route path="/apartmentshow" component={ApartmentShow} />
-            <Route path="/apartmentnew" component={ApartmentNew} />
-            <Route path="/apartmentedit" component={ApartmentEdit} />
-            <Route component={NotFound}/>
-          </Switch>
+        <Header {...this.props} />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/apartmentindex" render={(props)=> <ApartmentIndex listings={this.state.apartments}/>} /> 
+              <Route path="/apartmentshow" component={ApartmentShow} />
+              <Route path="/apartmentnew" component={ApartmentNew} />
+              <Route path="/apartmentedit" component={ApartmentEdit} />
+              <Route component={NotFound}/>
+            </Switch>
+          <Footer />
         </Router>
-        
-        
-  
+
     )
   }
 }
--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-
+import {Container} from 'reactstrap'
 export default class Home extends Component {
   render() {
     return (
-      <h3>Home</h3>
+      <Container>
+        <h3>Home</h3>
+      </Container>
     )
   }
 }
